8f1c45646a688ed27b50ef4054677ee173149cef
Nehal-11 <105434404+Nehal-11@users.noreply.github.com>
2022-05-13 16:44:33 +0530

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,15 @@
+package arrayList;
+
+public class Book {
+int id, quantity;
+String bookName, author, publisher;
+public Book(String bookName ,String author, String publisher, int id, int quantity ) {
+	// TODO Auto-generated constructor stub
+this.author = author;
+this.bookName = bookName;
+this.publisher = publisher;
+this.id = id;
+this.quantity = quantity;
+
+}
+}
